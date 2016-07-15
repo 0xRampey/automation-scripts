fd4619e9fc9d9702677f38b256cf79b1a702baaa
Prudhvi Rampey <gergteg777@gmail.com>
2016-07-15 18:58:05 +0530

    Added wifi login automation

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
+
+-- Automatated wifi login script--
+
+local ssids={"BPGC-HOSTEL","BPGC-WIFI"}
+
+hs.wifi.watcher.new(function()
+	local connectedWifi=hs.wifi.currentNetwork()
+	if not connectedWifi then return end
+	for i=1, 2 do
+      if ssids[i] == connectedWifi then
+      	break
+      else
+      	return
+      end
+    end
+
+--Execute custom script that logs in with a POST request--
+	hs.execute("~/Documents/Scripts/Mine/bpgc_portal_login.sh")
+
+	hs.notify.new({title="Logged in to "+connectedWifi, informativeText=""}):send()
+	end):start()
+
+-- Later versions will check POST response to notify login states--
+

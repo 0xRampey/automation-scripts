67a15cd801bc2a429f74f11aa0ee5555e8efa918
Prudhvi Rampey <gergteg777@gmail.com>
2016-08-03 13:48:28 +0530

    Improved notifications and it nows check for an internet connection before sending a request

--- init.lua
+++ init.lua
@@ -1,24 +1,44 @@
 
--- Automatated wifi login script--
+-- Automated wifi login script--
 
-local ssids={"BPGC-HOSTEL","BPGC-WIFI"}
+local ssids={"BPGC-HOSTEL","BPGC-WIFI","Prudhvi's Network"}
+ 
 
 hs.wifi.watcher.new(function()
-	local connectedWifi=hs.wifi.currentNetwork()
+	connectedWifi=hs.wifi.currentNetwork()
 	if not connectedWifi then return end
-	for i=1, 2 do
+	for i=1,3 do
       if ssids[i] == connectedWifi then
-      	break
-      else
-      	return
+    -- Check for an internet connection and then, send a request
+      hs.network.reachability.internet():setCallback(function(self, flags)
+        if (flags & hs.network.reachability.flags.reachable) > 0 then
+            -- a default route exists, so an active internet connection is present
+            login()
+        else
+            -- no default route exists, so no active internet connection is present
+        end
+   end):start()
+        -- No need to pause the watcher as it conveniently sends a request when an internet connection resets
       end
-    end
+  end
+    return
+end):start()
+-- Execute custom script that logs in with a POST request --
+function login()
+  	
+    local login = hs.task.new("~/Documents/Scripts/Mine/bpgc_portal_login.sh", function(exitCode,stdOut,stdErr) 
+      if(exitCode==0) then
+        hs.notify.new({title="Login succesfull", informativeText=("Logging in to " .. connectedWifi)}):send()
+      else
+        hs.notify.new({title=("Login succesfull"), informativeText="The server is down or you might want to authenticate."}):send()
+        end
+    end):start()
+  -- Debugging purposes
+    print(login)
 
---Execute custom script that logs in with a POST request--
-	hs.execute("~/Documents/Scripts/Mine/bpgc_portal_login.sh")
+	  
+  
+end
 
-	hs.notify.new({title="Logged in to "+connectedWifi, informativeText=""}):send()
-	end):start()
-
--- Later versions will check POST response to notify login states--
+-- Later versions will check POST response to notify login states
 
